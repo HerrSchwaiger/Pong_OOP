94327651c749841d102bb0ec21c0bdf522e93853
Schule <Schule@MacBook-Air-von-Tim-2.local>
2026-06-18 11:48:18 +0200

    fix jdk version and window name

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
         // Erstellen der Gameworld mit Breite 800 und Höhe 600
-        GameWorld world = new GameWorld("ShapeCraft 0.1", 800, 600);
+        GameWorld world = new GameWorld("Pong", 800, 600);
         // Hintergrund Farbe dunkel grau
         world.setBackground(Color.DARK_GRAY);
 
